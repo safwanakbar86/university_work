e5626c888fed4a0de4e926011661ef50664fe1d9
Safwan Akbar <122469916+safwanakbar86@users.noreply.github.com>
2024-03-24 14:35:55 +0500

    Create q3.c

---
+++ PDC/assgn_3/q3.c
@@ -0,0 +1,4 @@
+/*Implement a simple parallel linear search algorithm using OpenMP. The program works on an
+integer array of size ‘N’ filled with random numbers and a key-value to be searched. Write the
+parallel program to divide the search space among the multiple threads. Your program should
+output the total number of times the occurrence of the key found in the array.*/
