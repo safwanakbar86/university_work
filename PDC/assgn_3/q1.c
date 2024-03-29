2427ed735f83303d6cf79db8f563898d36434633
Safwan Akbar <122469916+safwanakbar86@users.noreply.github.com>
2024-03-29 12:50:44 +0500

    Update q1.c

--- PDC/assgn_3/q1.c
+++ PDC/assgn_3/q1.c
@@ -1,4 +1,3 @@
-//TBU
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -14,7 +13,7 @@ int main(int argc, char* argv[])
   int* odd = (int*)malloc(sizeof(int)* N);
   bool* prime = (bool*)malloc(sizeof(bool)* N);
   
-  printf("Odd Numbered Array:\n");
+  printf("Odd Numbers Array:\n");
   for (int i = 0; i < N; i++)
   {
     random_num = rand() % 10;
@@ -25,13 +24,14 @@ int main(int argc, char* argv[])
     printf("%d   ", random_num);
   }
   
-  #pragma omp parallel for num_threads(N) schedule(dynamic, 1)
+  #pragma omp parallel for num_threads(P) schedule(dynamic, 1)
   for (int i = 0; i < N; i++)
   {
     int square_root = odd[i] * odd[i];
     for (int j = 3; j < square_root; j++)
-      if (odd[i] % j == 0 && odd[i] != j)
-        prime[i] = false;
+      if (odd[i] != j)
+        if (odd[i] % j == 0)
+          prime[i] = false;
   }
   
   printf("\nPrime Odd Numbers:\n");
