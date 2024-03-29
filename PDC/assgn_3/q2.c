698dc6bd4016a1398636b80671cac0616f8cae5c
Safwan Akbar <122469916+safwanakbar86@users.noreply.github.com>
2024-03-29 19:06:12 +0500

    Update q2.c

--- PDC/assgn_3/q2.c
+++ PDC/assgn_3/q2.c
@@ -8,116 +8,115 @@ double end_time;
 
 int main()
 {
-  start_time = omp_get_wtime();
-  
-  // Code section to be parallelized
-  long int win = 0;
   int SIZE = 1000;
   int key = 111;
+  long int win = 0;
+  
+  start_time = omp_get_wtime();
   for (int i = 0; i < SIZE; i++)
-  {
     for (int j = 0; j < SIZE; j++)
-    {
       for (int k = 0; k < SIZE; k++)
       {
         int x = (i*i*1000/35) % 1000;
         int y = (j*j*1000/36) % 1000;
         int z = (k*k*1000/37) % 1000;
         if (key == (x+y+z))
-        {
           win=win+1;
-        }
       }
-    }
-  }
-  // Code section to be parallelized
-  
   end_time = omp_get_wtime();
   printf("Total wins = %ld\n", win);
   double sequential_time = end_time - start_time;
-  printf("Sequential Time: %f\n", sequential_time);
+  printf("Sequential Time: %f\n\n", sequential_time);
   
   
-  start_time = omp_get_wtime();
   win = 0;
-  #pragma omp parallel for num_threads(2) private(win) shared(key, SIZE) reduction(+:win)
+  start_time = omp_get_wtime();
+  #pragma omp parallel for num_threads(2) shared(win)
   for (int i = 0; i < SIZE; i++)
   {
     for (int j = 0; j < SIZE; j++)
     {
+      int private_win = 0;
       for (int k = 0; k < SIZE; k++)
       {
-        #pragma omp critical
         int x = (i*i*1000/35) % 1000;
         int y = (j*j*1000/36) % 1000;
         int z = (k*k*1000/37) % 1000;
         if (key == (x+y+z))
-        {
-          win=win+1;
-        }
+          private_win++;
+      }
+      #pragma omp critical
+      {
+        win += private_win;
       }
     }
   }
   end_time = omp_get_wtime();
   printf("Total wins = %ld\n", win);
+  
   double parallel_2_time = end_time - start_time;
   printf("Parallel Time using 2 threads: %f\n", parallel_2_time);
+  printf("Achieved speedup using 2 threads: %f\n\n", sequential_time / parallel_2_time);
   
   
-  start_time = omp_get_wtime();
   win = 0;
-  #pragma omp parallel for num_threads(3) private(win) shared(key, SIZE) reduction(+:win)
+  start_time = omp_get_wtime();
+  #pragma omp parallel for num_threads(3) shared(win)
   for (int i = 0; i < SIZE; i++)
   {
     for (int j = 0; j < SIZE; j++)
     {
+      int private_win = 0;
       for (int k = 0; k < SIZE; k++)
       {
-        #pragma omp critical
         int x = (i*i*1000/35) % 1000;
         int y = (j*j*1000/36) % 1000;
         int z = (k*k*1000/37) % 1000;
         if (key == (x+y+z))
-        {
-          win=win+1;
-        }
+          private_win++;
+      }
+      #pragma omp critical
+      {
+        win += private_win;
       }
     }
   }
   end_time = omp_get_wtime();
   printf("Total wins = %ld\n", win);
+  
   double parallel_3_time = end_time - start_time;
   printf("Parallel Time using 3 threads: %f\n", parallel_3_time);
+  printf("Achieved speedup using 3 threads: %f\n\n", sequential_time / parallel_3_time);
   
   
-  start_time = omp_get_wtime();
   win = 0;
-  #pragma omp parallel for num_threads(4) private(win) shared(key, SIZE) reduction(+:win)
+  start_time = omp_get_wtime();
+  #pragma omp parallel for num_threads(4) shared(win)
   for (int i = 0; i < SIZE; i++)
   {
     for (int j = 0; j < SIZE; j++)
     {
+      int private_win = 0;
       for (int k = 0; k < SIZE; k++)
       {
-        #pragma omp critical
         int x = (i*i*1000/35) % 1000;
         int y = (j*j*1000/36) % 1000;
         int z = (k*k*1000/37) % 1000;
         if (key == (x+y+z))
-        {
-          win=win+1;
-        }
+          private_win++;
+      }
+      #pragma omp critical
+      {
+        win += private_win;
       }
     }
   }
   end_time = omp_get_wtime();
   printf("Total wins = %ld\n", win);
+  
   double parallel_4_time = end_time - start_time;
   printf("Parallel Time using 4 threads: %f\n", parallel_4_time);
-  
-  printf("Achieved speedup using 2 threads: %f\n", sequential_time / parallel_2_time);
-  printf("Achieved speedup using 3 threads: %f\n", sequential_time / parallel_3_time);
-  printf("Achieved speedup using 4 threads: %f\n", sequential_time / parallel_4_time);
+  printf("Achieved speedup using 4 threads: %f\n\n", sequential_time / parallel_4_time);
 
   return 0;
 }
