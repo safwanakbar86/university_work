3652cb2a95a7e3fd218e52591c9118baa693e782
Safwan Akbar <122469916+safwanakbar86@users.noreply.github.com>
2024-04-29 22:20:10 +0500

    Update q1.c

--- PDC/assgn_4/q1.c
+++ PDC/assgn_4/q1.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
 #include <math.h>
 
-void one_to_all_broadcast(int dimension, int ID)
+void one_to_all_broadcast(int dimension, int node)
 {
 	int mask = (int)(pow(2, dimension) - 1);
 	for (int i = dimension - 1; i >= 0; i--)
 	{
 		mask = mask ^ (int)pow(2, i);
 
-		if ((ID & mask) == 0)
+		if ((node & mask) == 0)
 		{
-			if ((ID & (int)pow(2, i)) == 0)
+			if ((node & (int)pow(2, i)) == 0)
 			{
-				int dest = ID ^ (int)pow(2, i);
-				// send message to dest
-				printf("Node %d sends message to destination node %d in step %d\n", ID, dest, dimension - i);
+				int des = node ^ (int)pow(2, i);
+				printf("Node %d sends message to destination node %d in step %d\n", node, des, dimension - i);
 			}
 
 			else
 			{
-				int src = ID ^ (int)pow(2, i);
-				// receive message from src
-				printf("Node %d receives message from source node %d in step %d\n", ID, src, dimension - i);
+				int src = node ^ (int)pow(2, i);
+				printf("Node %d receives message from source node %d in step %d\n", node, src, dimension - i);
 			}
 		}
 	}
@@ -29,8 +27,8 @@ void one_to_all_broadcast(int dimension, int ID)
 
 int main()
 {
-        int dimension = 3;
-        int nodes = (int)pow(2, dimension);
+    int dimension = 3;
+    int nodes = (int)pow(2, dimension);
         
 	for (int j = 0; j < nodes; j++)
 	{
