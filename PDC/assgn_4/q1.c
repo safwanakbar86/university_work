1ac4a78d3911c1e07a842e739804cf6fc2e04652
Safwan Akbar <122469916+safwanakbar86@users.noreply.github.com>
2024-04-29 22:31:49 +0500

    Update q1.c

--- PDC/assgn_4/q1.c
+++ PDC/assgn_4/q1.c
@@ -7,7 +7,7 @@ void one_to_all_broadcast(int dimension, int node)
 	for (int i = dimension - 1; i >= 0; i--)
 	{
 		mask = mask ^ (int)pow(2, i);
-
+	
 		if ((node & mask) == 0)
 		{
 			if ((node & (int)pow(2, i)) == 0)
@@ -15,7 +15,7 @@ void one_to_all_broadcast(int dimension, int node)
 				int dst = node ^ (int)pow(2, i);
 				printf("Node %d sends message to destination node %d in step %d\n", node, dst, dimension - i);
 			}
-
+	
 			else
 			{
 				int src = node ^ (int)pow(2, i);
@@ -27,13 +27,13 @@ void one_to_all_broadcast(int dimension, int node)
 
 int main()
 {
-    int dimension = 3;
-    int nodes = (int)pow(2, dimension);
+	int dimension = 3;
+	int nodes = (int)pow(2, dimension);
         
 	for (int j = 0; j < nodes; j++)
 	{
 		one_to_all_broadcast(dimension, j);
-	        printf("\n");	
+		printf("\n");	
 	}
 	
 	return 0;
